1ad825fb190751b90cb5d4ce4a096b4d8f1fab62
Vanessa Bautista <vanessa@digit4l.co>
2022-10-05 14:47:50 -0500

    Primera publicación

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,13 @@
+/**
+ * ======== Código de TypeScript ==========
+ */
+
+ let nombre: string = 'Vane';
+ let hp: number | string = 25;
+ let estaVivo: boolean = true;
+ 
+ hp = 25;
+ 
+ console.log(nombre, hp, estaVivo);
+ 
+ 
