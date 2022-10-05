c1f7068e0217ec6e076837df11b3cdf5b7c24013
Vanessa Bautista <vanessa@digit4l.co>
2022-10-05 14:50:21 -0500

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
 /**
  * ======== Código de TypeScript ==========
  */
-// let y = 1;
-// if (y === 1) {
-//     let x = y + 1;
-//     console.log(x);
-// }
-
-// let obj = {
-//     id: 1,
-//     name: 'Jose'
-// }
-
-// let obj2 = obj;
-// obj2.name = 'Carlos';
-
-// let x = obj.name;
-// console.log(x);
-
-let arr = [8,6,0,12,20,11];
-let x = arr.filter(n => n > 7)[1]
-
-console.log(x);
-
-
-
